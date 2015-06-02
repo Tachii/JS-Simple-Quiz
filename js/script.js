cee5c99e1a2c96781eab0411999489414db14737
Tachi <zaveruha007@gmail.com>
2015-06-02 13:46:54 +0300

    Initial Upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
-function sumbitAnswers(){
+function submitAnswers(){
 	var total = 5;
-	var scroe = 0;
+	var score = 0;
 	
 	//Get User Input
+	var q1 = document.forms["quizForm"]["q1"].value;
+	var q2 = document.forms["quizForm"]["q2"].value;
+	var q3 = document.forms["quizForm"]["q3"].value;
+	var q4 = document.forms["quizForm"]["q4"].value;
+	var q5 = document.forms["quizForm"]["q5"].value;
+	
+	//Validation 
+	if(q1 == null || q1 == ' '){
+		alert('You missed question 1');
+		return false;
+	}
 	
 }
