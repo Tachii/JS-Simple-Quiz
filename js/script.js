3c0bbe1eee15737f4b6711e6e37c75a3707959eb
Tachi <zaveruha007@gmail.com>
2015-06-02 14:28:51 +0300

    Initial Upload

--- js/script.js
+++ js/script.js
@@ -2,17 +2,13 @@ function submitAnswers(){
 	var total = 5;
 	var score = 0;
 	
-	//Get User Input
-	var q1 = document.forms["quizForm"]["q1"].value;
-	var q2 = document.forms["quizForm"]["q2"].value;
-	var q3 = document.forms["quizForm"]["q3"].value;
-	var q4 = document.forms["quizForm"]["q4"].value;
-	var q5 = document.forms["quizForm"]["q5"].value;
+	for (i = 1; i <=total; i++) {
+	    if(document.forms["quizForm"]["q" + i].value == ""){
+	        alert("You missed question " + i);
+	        return false;
+	    } 
+	    score += document.forms["quizForm"]["q" + i].value;
+	 }
 	
-	//Validation 
-	if(q1 == null || q1 == ' '){
-		alert('You missed question 1');
-		return false;
-	}
 	
 }
